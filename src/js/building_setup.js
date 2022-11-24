b612cda59d8e441b919c1dc2e5a4edceb00dfc25
Kabutokohi <56236014+Kabutokohi@users.noreply.github.com>
2022-11-24 19:28:37 -0300

    modified:   src/js/building_setup.js 	modified:   src/js/marker_setup.js

--- src/js/building_setup.js
+++ src/js/building_setup.js
@@ -92,7 +92,63 @@ var edF = L.polygon(
 ]
 ]], {color: "#5709b0"}).addTo(map);
 
+//Definicaion de los pasilos del edificio A piso 1
+var pathAP1S1 = L.polyline([[1402,3633],[1352,3348]],{color:"black"}).addTo(map); //Corredor al lado de dfis
+var pathAP1S1_1 = L.polyline([[1444,3549],[1410,3337]],{color:"black"}).addTo(map); //Corredor al lado de dfis
+var pathAP1S1_2 = L.polyline([[1956,3659],[1930,3510]],{color:"black"}).addTo(map); //Corredor al lado de dfis
+var pathAP1S1_3 = L.polyline([[2072,3633],[2048,3483]],{color:"black"}).addTo(map); //Corredor al lado de dfis
+//Division por salas PRIMERA MITAD
+var pathAP1S1_3Salas1 = L.polyline([[1567,3742],[1541,3601]],{color:"black"}).addTo(map); //ASEM
+var pathAP1S1_3Salas2 = L.polyline([[1630,3731],[1606,3587]],{color:"black"}).addTo(map); //DIRECCION AMBIENTAL
+var pathAP1S1_3Salas3 = L.polyline([[1683,3716],[1652,3576]],{color:"black"}).addTo(map); //A-025
+var pathAP1S1_3Salas4 = L.polyline([[1807,3691],[1785,3542]],{color:"black"}).addTo(map); //A-026
+var pathAP1S1_3Salas5 = L.polyline([[1750,3704],[1730,3557]],{color:"black"}).addTo(map); //A-027
+var pathAP1S1_3Salas6 = L.polyline([[1864,3675],[1850,3527]],{color:"black"}).addTo(map); //A-028
+var pathAP1S1_3Salas7 = L.polyline([[1916,3668],[1894,3517]],{color:"black"}).addTo(map); //130-B
+//Division por salas SEGUNDA MITAD
+var pathAP1S1_3Salas = L.polyline([[2072,3633],[2048,3483]],{color:"black"}).addTo(map); //A-012
+var pathAP1S1_3Salas = L.polyline([[2072,3633],[2048,3483]],{color:"black"}).addTo(map); //A-013
+var pathAP1S1_3Salas = L.polyline([[2072,3633],[2048,3483]],{color:"black"}).addTo(map); //A-014
+var pathAP1S1_3Salas = L.polyline([[2072,3633],[2048,3483]],{color:"black"}).addTo(map); //A-015
+var pathAP1S1_3Salas = L.polyline([[2072,3633],[2048,3483]],{color:"black"}).addTo(map); //ADMINISTRATIVOS
+var pathAP1S1_3Salas = L.polyline([[2072,3633],[2048,3483]],{color:"black"}).addTo(map); // DGIIP/UCP
+var pathAP1S1_3Salas = L.polyline([[2072,3633],[2048,3483]],{color:"black"}).addTo(map); //PACE
+//SAlas A Interiores
+var sepAP1S2 = L.polyline([[1627,3156],[1598,3003]],{color:"black"}).addTo(map); //Corredor al lado de dfis
+var sepAP1S3 = L.polyline([[1310,3224],[1282,3074]],{color:"black"}).addTo(map); //Corredor al lado de dfis
+var sepAP1S4 = L.polyline([[1615,3085],[1967,3008]],{color:"black"}).addTo(map); //Corredor al lado de dfis
+var sepAP1S5 = L.polyline([[1953,2921],[1980,3082]],{color:"black"}).addTo(map); //Corredor al lado de dfis
+var sepAP1S6 = L.polyline([[2655,3342],[1460,3619]],{color:"black"}).addTo(map);
+var sepAp1S7 = L.polyline([[2636,3275],[1441,3549]],{color:"black"}).addTo(map);
+var divSalaA16_1 = L.polyline([[1449,3322],[1490,3537]],{color:"black"}).addTo(map);
+var sepAP1S8 = L.polyline([[2705,3368],[2731,3487]],{color:"black"}).addTo(map);
+var sepAP1S9 = L.polyline([[2561,3366],[2580,3521]],{color:"black"}).addTo(map);
+var sepAP1S10 = L.polyline([[2663,3375],[2688,3496]],{color:"black"}).addTo(map);
+var sepAP1S11 = L.polyline([[2673,3435],[2574,3457]],{color:"black"}).addTo(map);
+var sepAP1S12 = L.polyline([[2625,3445],[2640,3507]],{color:"black"}).addTo(map);
+var sepAP1S13 = L.polyline([[2524,3375],[2544,3530]],{color:"black"}).addTo(map);
+var sepAP1S14 = L.polyline([[2346,3573],[2312,3420]],{color:"black"}).addTo(map);
+var sepAP1S15 = L.polyline([[2463,3548],[2433,3393]],{color:"black"}).addTo(map);
+var sepAP1S16 = L.polyline([[2219,3602],[2191,3450]],{color:"black"}).addTo(map);
+var sepAP1S17 = L.polyline([[2450,3282],[2629,3240]],{color:"black"}).addTo(map);
+//Separacion de baños parte exterior salas A
+var pathBanoI = L.polyline([[1864,3232],[1896,3223]],{color:"black"}).addTo(map);
+var pathBanoD = L.polyline([[1977,3203],[2018,3414]],{color:"black"}).addTo(map);
 
+//salas A exteriores
+var sepAN1 = L.polyline([[1716,3483],[1679,3267]],{color:"black"}).addTo(map);
+var sepAN2 = L.polyline([[1534,3306],[1568,3522]],{color:"black"}).addTo(map);
+var sepAN3 = L.polyline([[1864,3230],[1904,3442]],{color:"black"}).addTo(map);
+var sepAN4 = L.polyline([[1756,3257],[1795,3464]],{color:"black"}).addTo(map);
+var sepAN5 = L.polyline([[2090,3181],[2132,3395]],{color:"black"}).addTo(map);
+var sepAN6 = L.polyline([[2203,3155],[2240,3365]],{color:"black"}).addTo(map);
+var sepAN7 = L.polyline([[2315,3130],[2350,3339]],{color:"black"}).addTo(map);
+var sepAN8 = L.polyline([[2424,3106],[2456,3316]],{color:"black"}).addTo(map);
+var sepAN9 = L.polyline([[2286,3008],[2258,2859]],{color:"black"}).addTo(map);
+var sepAN10 = L.polyline([[2451,2965],[2430,2819]],{color:"black"}).addTo(map);
+
+//separacion interna DMAT
+var sepDmat = L.polyline([[1962,2988],[1611,3067]],{color:"black"}).addTo(map);
 //Tooltips
 
 //? buscar alternativa
--- src/js/marker_setup.js
+++ src/js/marker_setup.js
@@ -21,6 +21,73 @@ markers.push([salaE003, "Sala E203"])
 var lab_metro = L.marker([3239,562], riseOnHover = true, interactive = true);
 markers.push([lab_metro, "Laboratorio Metrología"])
 
+//Edificio A Piso 1
+var dep_fis = L.marker([1281, 3355], riseOnHover = true, interactive = true);
+markers.push([dep_fis, "Departamento de fisica"])
+var salaA016 = L.marker([1427,3367], riseOnHover = true, interactive = true);
+markers.push([salaA016, "Sala A016"])
+var dep_mat = L.marker([1459, 3095], riseOnHover = true, interactive = true);
+markers.push([dep_mat, "Departamento de Matematica"])
+var dep_eh = L.marker([1814, 3081], riseOnHover = true, interactive = true);
+markers.push([dep_eh, "Departamento de Estudios humanisticos"])
+var dep_qui = L.marker([1824, 2991], riseOnHover = true, interactive = true);
+markers.push([dep_qui, "Departamento de Quimica"])
+var asem = L.marker([1525, 3680], riseOnHover = true, interactive = true);
+markers.push([asem, "ASEM"])
+var ingAmb = L.marker([1586, 3668], riseOnHover = true, interactive = true);
+markers.push([ingAmb, "Ingieneria Civil Ambiental"])
+var salaA025 = L.marker([1645, 3658], riseOnHover = true, interactive = true);
+markers.push([salaA025, "Sala A-025"])
+var salaA026 = L.marker([1701, 3640], riseOnHover = true, interactive = true);
+markers.push([salaA026, "Sala A-026"])
+var salaA027 = L.marker([1770, 3628], riseOnHover = true, interactive = true);
+markers.push([salaA027, "Sala A-027"])
+var salaA028 = L.marker([1829, 3609], riseOnHover = true, interactive = true);
+markers.push([salaA028, "Sala A-028"])
+var salaA029 = L.marker([1926, 3592], riseOnHover = true, interactive = true);
+markers.push([salaA029, "Sala A-029"])
+var sala130B = L.marker([1880, 3597], riseOnHover = true, interactive = true);
+markers.push([sala130B, "Sala 130-B"])
+var banoMiA = L.marker([1949, 3326], riseOnHover = true, interactive = true);
+markers.push([banoMiA, "Baño"])
+var salaA017 = L.marker([2000, 2989], riseOnHover = true, interactive = true);
+markers.push([salaA017, "Sala A-017"])
+var salaA001 = L.marker([2189,2950], riseOnHover = true, interactive = true);
+markers.push([salaA001, "Sala A-001"])
+var salaA002 = L.marker([2348,2917], riseOnHover = true, interactive = true);
+markers.push([salaA002, "Sala A-002"])
+var salaA003 = L.marker([2492,2887], riseOnHover = true, interactive = true);
+markers.push([salaA003, "Sala A-003"])
+var salaA008 = L.marker([2058,3301], riseOnHover = true, interactive = true);
+markers.push([salaA008, "Sala A-008"])
+var salaA009 = L.marker([2168,3264], riseOnHover = true, interactive = true);
+markers.push([salaA009, "Sala A-009"])
+var salaA010 = L.marker([2283,3237], riseOnHover = true, interactive = true);
+markers.push([salaA010, "Sala A-010"])
+var salaA011 = L.marker([2391,3228], riseOnHover = true, interactive = true);
+markers.push([salaA011, "Sala A-011"])
+var banoH = L.marker([2533,3161], riseOnHover = true, interactive = true);
+markers.push([banoH, "Baño de Hombres"])
+var banoM = L.marker([2562,3275], riseOnHover = true, interactive = true);
+markers.push([banoM, "Baño de Mujeres"])
+var caja = L.marker([2744,3410], riseOnHover = true, interactive = true);
+markers.push([caja, "Caja de Pagos"])
+var PACE = L.marker([2653,3480], riseOnHover = true, interactive = true);
+markers.push([PACE, "PACE"])
+var DGIIP = L.marker([2603,3486], riseOnHover = true, interactive = true);
+markers.push([DGIIP, "DGIIP/UCP"])
+var admin = L.marker([2554,3450], riseOnHover = true, interactive = true);
+markers.push([admin, "Administrativos"])
+var salaA015 = L.marker([2490,3458], riseOnHover = true, interactive = true);
+markers.push([salaA015, "Sala A-015"])
+var salaA014 = L.marker([2385,3488], riseOnHover = true, interactive = true);
+markers.push([salaA014, "Sala A-014"])
+var salaA013 = L.marker([2264,3517], riseOnHover = true, interactive = true);
+markers.push([salaA013, "Sala A-013"])
+var salaA012 = L.marker([2126,3542], riseOnHover = true, interactive = true);
+markers.push([salaA012, "Sala A-012"])
+
+
 //! Placeholder para testeo, eventualmente se borrará/cambiará
 var gimnasio = L.marker([2427,1605], riseOnHover = true, interactive = true);
 markers.push([gimnasio, "Gimnasio"])
