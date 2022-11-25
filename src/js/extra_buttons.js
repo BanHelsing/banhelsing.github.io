df6874e8d9e86f9bcac874df7161e28d60109664
Ian <mrjoystickerox@gmail.com>
2022-11-25 18:49:07 -0300

    Selector de pisos

--- src/js/extra_buttons.js
+++ src/js/extra_buttons.js
@@ -1,10 +1,33 @@
 //Salas de clases
 //Esconde o muestra las salas como corresponde
 L.easyButton('bi bi-person-video3', function(){
-    //map.removeLayer(salas_list);
-    if (map.hasLayer(salas_layer)) {
-        salas_layer.remove();
-    } else {
-        salas_layer.addTo(map);
-    }
+    var salas_layers = [salas_piso1_layer, salas_piso2_layer];
+    salas_layers.forEach(layer => {
+        if (map.hasLayer(layer)) {
+            layer.remove();
+        } else {
+            layer.addTo(map);
+        }
+    });
 }).addTo(map)
+
+
+//Admins
+//Esconde o muestra las oficinas como corresponde
+L.easyButton('bi bi-building', function(){
+    var admin_layers = [admin_piso1_layer];
+    admin_layers.forEach(layer => {
+        if (map.hasLayer(layer)) {
+            layer.remove();
+        } else {
+            layer.addTo(map);
+        }
+    });
+}).addTo(map)
+
+var baseMaps = {
+    "Piso 1": piso1_layer,
+    "Piso 2": piso2_layer
+};
+
+L.control.layers(baseMaps).addTo(map);
